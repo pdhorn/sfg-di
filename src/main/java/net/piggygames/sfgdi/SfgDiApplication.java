c0d189550f1d4ecb7a92cb396a11f5caae798dff
Peter Horn <pdhorn@gmail.com>
2021-09-26 15:39:38 -0500

    spring-di, qualifier

--- src/main/java/net/piggygames/sfgdi/SfgDiApplication.java
+++ src/main/java/net/piggygames/sfgdi/SfgDiApplication.java
@@ -1,13 +1,46 @@
 package net.piggygames.sfgdi;
 
+import net.piggygames.sfgdi.controllers.ConstructorInjectedController;
+import net.piggygames.sfgdi.controllers.MyController;
+import net.piggygames.sfgdi.controllers.PropertyInjectedController;
+import net.piggygames.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
+import org.springframework.context.ApplicationContext;
+import org.yaml.snakeyaml.introspector.Property;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
+		System.out.println("---------- Property");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("---------- Setter");
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("---------- Constructor");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
+
 	}
 
 }
---
+++ src/main/java/net/piggygames/sfgdi/services/GreetingService.java
@@ -0,0 +1,5 @@
+package net.piggygames.sfgdi.services;
+
+public interface GreetingService {
+    String sayGreeting();
+}
